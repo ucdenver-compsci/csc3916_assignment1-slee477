3b11a072f214b0e25ddcef9517ab1e51826d55f6
slee477 <lee.steffi47@gmail.com>
2024-02-03 20:29:19 -0700

    add start command to express app

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
-var server = require("http").createServer();
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
-server.on("request", (request, response) => {
-    var body = [];
-    request.on("data", chunk => {
-        body.push(chunk);
-    });
-    request
-        .on("end", () => {
-            let bodyString = body.concat().toString();
-            console.log(bodyString);
-            response.end(bodyString);
-        })
-        .on("error", () => {
-            response.statusCode = 400;
-            response.end();
-        });
-    response.on("error", err => {
-        console.error(err);
-    });
-});
-server.listen(process.env.PORT || 8008, () => {
-    console.log("Server listening at 8008");
+app.use(bodyParser.text({type: '*/*'}));
+
+app.post('/', function(req,res){
+
+    var body = req.body
+
+    if (req.get('Content-Type')){
+        console.log(req.get('Content-Type'));
+       res = res.type(req.get('Content-Type'));
+    }
+
+    res.send (body)
 });
 
-module.exports = server; // for testing
+app.listen(process.env.PORT || 8008, function(){
+    console.log('Listening on port %d', this.address().port);
+});
+
+module.exports = app; // for testing
 
 //curl -d "echo" -H "Content-Type: text" -X POST http://localhost:8008
